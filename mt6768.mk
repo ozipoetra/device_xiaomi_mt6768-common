fa6e46995a1b5333d49bedc564c4a0728add839e
krasCGQ <krasCGQ@users.noreply.github.com>
2023-01-04 23:59:29 +0700

    mt6768-common: Inherit several Android Go configurations Beneficial for everyone, even on non-Go devices.
    Signed-off-by: 01soni247 <01soni247@gmail.com>

--- mt6768.mk
+++ mt6768.mk
@@ -195,6 +195,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0.vendor
