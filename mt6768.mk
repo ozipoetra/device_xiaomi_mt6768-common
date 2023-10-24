9adee21d435db36893f12d7961ac27b3744bc71d
SamarV-121 <samar@samarv121.dev>
2023-10-24 11:45:36 +0000

    mt6768-common: Fix libsensorndkbridge * realme-mt6785-devs/android_hardware_mediatek@67d7558
    Change-Id: I842132b94cb59b5f72078873eb35bded5d76870c
    Signed-off-by: 01soni247 <01soni247@gmail.com>

--- mt6768.mk
+++ mt6768.mk
@@ -359,7 +359,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    libsensorndkbridge
+    libsensorndkbridge_mtk
 
 # Shims
 PRODUCT_PACKAGES += \
