65b9c806061a351fbdb1e1e7310c2882a1d10f72
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2024-01-09 09:48:24 +0700

    mt6768-common: update remove package list

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -6,7 +6,9 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
     Seedvault \
-    Jelly
+    Jelly \
+    bcr \
+    Papers
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
